9696910ffeb24c2651338b72b7967eb0be83ee20
Pedro Teixeira <pedro.teixeira@gmail.com>
2011-10-18 15:19:56 +0100

    now with monitor for monitoring timeouts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var redis = require('redis')
-  , EventEmitter = require('events').EventEmitter;
+  , EventEmitter = require('events').EventEmitter
+  , Monitor = require('./monitor');
 
 module.exports = function(config) {
   var connections = []
@@ -9,7 +10,8 @@ module.exports = function(config) {
   
   config = config || {};
   
-  config.popTimeout || (config.popTimeout = 5);
+  config.popTimeout || (config.popTimeout = 5); // 5 seconds
+  config.workerTimeout || (config.workerTimeout = 60 * 1000);
   
   function createClient() {
     var client = redis.createClient(config.port || 6379, config.host || '127.0.0.1');
@@ -55,35 +57,57 @@ module.exports = function(config) {
       if (err) { return error(err); }
       
       var jobId = 'jobs:' + nextId;
-
-      client.set(jobId, JSON.stringify(job), function(err) {
-        if (err) { return error(err); }
-        client.lpush(inactiveQueueName(type), nextId, function(err) {
+      
+      client
+        .multi()
+        .hset(jobId, 'job', JSON.stringify(job))
+        .hset(jobId, 'created', Date.now())
+        .lpush(inactiveQueueName(type), nextId)
+        .exec(function(err) {
           if (err) { return error(err); }
           client.end();
           callback();
         });
-      });
     });
     
   };
   
   function connect(type, callback) {
-    var connection = connections[type];
+    var connection = connections[type]
+      , client
+      , monitor;
     try {
-      if (! connection || ! connection.connected) {
-        connections[type] = connection = createClient();
+      if (! connection) {
+        client = createClient();
+        monitor = Monitor(createClient(), inactiveQueueName(type), activeQueueName(type), config.workerTimeout, config.monitorInterval);
+        monitor.on('error', function(err) {
+                         that.emit('err', err);
+                       });
+                       monitor.on('timeout', function(job) {
+                         that.emit('timeout', job);
+                       });
+        connections[type] = connection = {
+            client: client
+          , monitor: monitor
+        };
       }
     } catch(err) {
+      console.error(err);
       return callback(err);
     }
-    callback(null, connection);
+    callback(null, connection.client);
   }
   
   function disconnect(type) {
     var connection = connections[type];
-    if (connection && connection.connected) {
-      connection.end();
+    if (connection) {
+      if (connection.client && connection.client.connected) {
+        connection.client.end();
+      }
+      if (connection.monitor) {
+        connection.monitor.end();
+      }
+      delete connections[type];
     }
   }
   
@@ -92,19 +116,26 @@ module.exports = function(config) {
       if (err) { return that.emit('error', err); }
       var activeName = activeQueueName(type) ;
       client.brpoplpush(inactiveQueueName(type), activeName, config.popTimeout, function(err, jobId) {
-
+        var jobDbId;
         if (err) { return that.emit('error', err); }
         
         if (! jobId) { return callback(null, null); }
+        
+        jobDbId = 'jobs:' + jobId;
 
-        client.get('jobs:' + jobId, function(err, jobStr) {
+        client.hget(jobDbId, 'job', function(err, jobStr) {
           if (err) { return that.emit(err); }
 
           callback(JSON.parse(jobStr), function(cb) {
-            client.lrem(activeName, 1, jobId, function(err) {
-              if (err) { return that.emit(err); }
-              cb();
-            });
+            client
+              .multi()
+              .lrem(activeName, 1, jobId)
+              .hdel(jobDbId, 'job')
+              .hdel(jobDbId, 'created')
+              .exec(function(err) {
+                if (err) { return that.emit(err); }
+                cb();
+              });
           });
         });
       });
